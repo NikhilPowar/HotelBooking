937fc30c781719b4e5089e32815a23e2d3e508a2
NikhilPowar <nikhilpowar16@gmail.com>
2017-02-12 17:31:18 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
-   define('DB_SERVER', 'localhost:3036');
-   define('DB_NAME', 'hotelsystem');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'rootpassword');
-   define('DB_DATABASE', 'database');
-   $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   $db = mysqli_select_db($con, DB_NAME);
-?>
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'dummy');
+   define('DB_PASSWORD', 'dummy');
+   define('DB_DATABASE', 'hotelbooking');
+   $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   if (!$conn) {
+     $_SESSION['login']="false";
+   }
+   else{
+     $_SESSION['login']="true";
+   }
+  ?>
