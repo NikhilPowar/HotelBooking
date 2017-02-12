336113c4c1ee6cb686c26073c2d9b0a3ea17026e
NikhilPowar <nikhilpowar16@gmail.com>
2017-02-12 17:29:24 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,85 +1,61 @@
-<!DOCTYPE html>
 <html>
-<style>
-form {
-    border: 3px solid #f1f1f1;
-}
-
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
-}
-
-button {
-    background-color: #4CAF50;
-    color: white;
-    padding: 16px 24px;
-    margin: 8px 0;
-    border: none;
-    border-radius: 8px;
-    cursor: pointer;
-}
-
-.cancelbtn {
-    width: auto;
-    padding: 12px 22px;
-    background-color: #f44336;
-}
-
-.imgcontainer {
-    text-align: center;
-    margin: 24px 0 12px 0;
-}
-
-img.avatar {
-    width: 40%;
-    border-radius: 50%;
-}
-
-.container {
-    padding: 16px 200px;
-}
-
-span.psw {
-    float: right;
-    padding-top: 16px;
-}
-
-/* Change styles for span and cancel button on extra small screens */
-@media screen and (max-width: 300px) {
-    span.psw {
-       display: block;
-       float: none;
-    }
-    .cancelbtn {
-       width: 100%;
-    }
-}
-</style>
+<link rel="stylesheet" type="text/css" href="form.css">
 <body>
 
-<h2>Login Form</h2>
+<?php
+  $unameErr = $pswErr = "";
+  $uname = $psw = "";
+  session_start();
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $uname = test_input($_POST["uname"]);
+    if (!preg_match("/^[a-zA-Z ]*$/",$uname)) {
+      $unameErr = "Only letters and white space allowed";
+    }
 
-<form action="action_page.php">
+    $psw = test_input($_POST["psw"]);
+    if (!preg_match("/^[a-zA-Z]*$/",$psw)) {
+      $pswErr = "Only letters allowed";
+    }
+
+    if(strcmp($unameErr, "")==0 and strcmp($pswErr, "")==0){
+      $_SESSION['username']=$uname;
+      $_SESSION['password']=$psw;
+      include 'config.php';
+      header("Location: http://localhost/Project/homepage.php");
+    }
+  }
+
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+?>
+<form-header>
+<h2>Login Form</h2>
+<p align="center"><span class="error">* required field.</span></p>
+</form-header>
+<form-text>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
   <div class="container">
     <label><b>Username</b></label>
+    <span class="error">* <?php echo $unameErr;?></span>
     <input type="text" placeholder="Enter Username" name="uname" required>
 
     <label><b>Password</b></label>
+    <span class="error">* <?php echo $pswErr;?></span>
     <input type="password" placeholder="Enter Password" name="psw" required>
 
     <button type="submit">Login</button>
-  </div>
 
-  <div class="container" style="background-color:#f1f1f1">
     <button type="button" class="cancelbtn">Cancel</button>
+
+    <a href="signup.php">New User? Sign up here.</a>
   </div>
 </form>
+</form-text>
 
 </body>
 </html>
