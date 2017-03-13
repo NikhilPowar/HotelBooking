225661cbced905d4e92c40146edc5f272ffb5b4d
NikhilPowar <nikhilpowar16@gmail.com>
2017-03-13 15:46:20 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
 
 <?php
   $unameErr = $emailErr = $pswErr = $conpswErr = "";
-  $uname = $email = $psw = $conpsw = "";
+  $uname = $email = $psw = $conpsw = $role = "";
   session_start();
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $uname = test_input($_POST["uname"]);
@@ -27,16 +27,20 @@
       $conpswErr = "Password doesn't match";
     }
 
+    $role=$_POST["role"];
+
     if(strcmp($unameErr, "")==0 and strcmp($pswErr, "")==0 and strcmp($pswconErr, "")==0 and strcmp($emailErr, "")==0){
       $_SESSION['username']=$uname;
       $_SESSION['password']=$psw;
-      include 'config.php';
+      $_SESSION['role']=$role;
+      require 'config.php';
       $stmt = mysqli_stmt_init($conn);
-      if (mysqli_stmt_prepare($stmt, 'insert into `users` values( ?, ?, ?)')) {
-        mysqli_stmt_bind_param($stmt, 'sss', $name, $pw, $em);
+      if (mysqli_stmt_prepare($stmt, 'insert into `users` values( ?, ?, ?, ?)')) {
+        mysqli_stmt_bind_param($stmt, 'ssss', $name, $pw, $em, $r);
         $name=$uname;
         $pw=$psw;
         $em=$email;
+        $r=$role;
         mysqli_stmt_execute($stmt);
         mysqli_stmt_close($stmt);
       }
@@ -76,11 +80,16 @@
     <span class="error">* <?php echo $conpswErr;?></span>
     <input type="password" placeholder="Re-enter Password" name="conpsw" required>
 
+    <select name="role">
+      <option value="customer">Customer</option>
+      <option value="manager">Manager</option>
+    </select>
+
     <button type="submit">Sign Up</button>
 
     <button type="button" class="cancelbtn">Cancel</button>
 
-    <a href="login.php">Already have an account? Login Here.</a>
+    <a href="login.php">Have an account? Login Here.</a>
   </div>
 </form>
 </form-text>
