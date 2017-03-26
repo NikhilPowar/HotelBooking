a9d0c53fee65cb18d1665c2e33eaa467bcaa95cb
NikhilPowar <nikhilpowar16@gmail.com>
2017-03-26 11:03:08 +0530

    Update paysuccess.php

--- paysuccess.php
+++ paysuccess.php
@@ -1,5 +1,23 @@
 <?php
   session_start();
-  echo"Payment successful";
-
+  require 'config.php';
+  echo'Payment successful.<br>
+    Booking Details:<br>
+    Hotel name : '.$_SESSION['hname'].'<br>
+    From Date : '.$_SESSION['from'].'<br>
+    To Date   : '.$_SESSION['to'].'<br>
+    Amount Paid : '.$_SESSION['totalPrice'].'<br>
+    <a href="homepage.php">Click Here</a> to return home.
+  ';
+  $stmt = mysqli_stmt_init($conn);
+  if(mysqli_stmt_prepare($stmt, "insert ignore into history values(?, ?, ?, ?, ?)")){
+    mysqli_stmt_bind_param($stmt, "sissi", $uname, $hid, $from, $to, $totalPrice);
+    $uname=$_SESSION['username'];
+    $hid=$_SESSION['hid'];
+    $from=$_SESSION['from'];
+    $to=$_SESSION['to'];
+    $totalPrice=$_SESSION['totalPrice'];
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+  }
 ?>
